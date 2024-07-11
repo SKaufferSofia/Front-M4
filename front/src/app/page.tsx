ec8a23c17d6143e351a7d14dac68b046198c537f
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2024-07-11 20:30:43 -0300

    "Video carga"

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,10 +1,21 @@
-import LandingComponent from "@/components/landing/landing";
-import React from "react";
+import React, { Suspense } from "react";
+
+const LandingComponentV2 = React.lazy(
+  () => import("@/components/landing/landing")
+);
 
 const Landing = () => {
   return (
     <div>
-      <LandingComponent />
+      <Suspense
+        fallback={
+          <div>
+            <img src="/carousel-3.avif" alt="" />
+          </div>
+        }
+      >
+        <LandingComponentV2 />
+      </Suspense>
     </div>
   );
 };
